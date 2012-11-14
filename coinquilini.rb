c57720a03e74546a6949a637ac35584bf8bba84a
jibi <jibi@paranoici.org>
2012-11-14 20:56:50 +0100

    Added payments form

--- coinquilini.rb
+++ coinquilini.rb
@@ -23,8 +23,25 @@ end
 Users = DB[:users]
 Payments = DB[:payments]
 
-get '/' do
+module Sinatra
 
+module UserList
+	def user_list
+		list = '<select name=who>'
+		Users.each do |u|
+			list += "<option value='#{u[:id]}'>#{u[:name]}</option>"
+		end
+		list += '</select>'
+	end
+end
+
+end
+
+configure do
+	helpers Sinatra::UserList
+end
+get '/' do
+	erb :pay_form, {:locals => { :user_list => user_list }}
 end
 
 get '/show' do
