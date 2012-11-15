2a74cec283f82746f3b33be67ffd9c03fd500e98
jibi <jibi@paranoici.org>
2012-11-15 19:00:28 +0100

    Added payments list

--- coinquilini.rb
+++ coinquilini.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-#
+
 require 'sinatra'
 require 'sequel'
 
@@ -30,6 +30,8 @@ module Validate
 		  :error => 'Something\'s missing.',
 		  :msg => "Please specify #{name}" }}) if 
 		  params[param].nil? or params[param].empty?
+
+		  params[param]
 	end
 end
 end
@@ -43,20 +45,39 @@ get '/' do
 end
 
 get '/show' do
+	ind_tot	= Hash.new(0)
+	tot	= 0
 
+	period	= params["period"] || Time.now.strftime("%Y %m")
+	year	= period[0..3].to_i
+	month	= period[5..6].to_i
+	users_n	= Users.count
+
+	start_t = Time.mktime(year, month).to_i
+	end_t	= (month.eql?(12) ? Time.mktime(year + 1) :
+		  Time.mktime(year, month + 1)).to_i
+
+	payments = Payments.join(:users, :id => :who).where(:date => (start_t .. end_t))
+
+	payments.each do |p|
+		ind_tot[p[:who]] += p[:how].to_f.round 2
+		tot += p[:how].to_f.round 2
+	end
+
+	avg_tot = (tot / users_n).round 2 if users_n.nonzero?
+
+	erb :list, {:locals => { :ind_tot => ind_tot, :avg_tot => avg_tot,
+		:payments => payments }}
 end
 
 post '/' do
-	validate 'who', :who
-	validate 'what', :what
-	validate 'how', :how
-
 	Payments.insert(
-		:who	=> params[:who], 
-		:what	=> params[:what], 
-		:how	=> params[:how].gsub(',', '.'),
+		:who	=> validate('who',  :who), 
+		:what	=> validate('what', :what), 
+		:how	=> validate('how',  :how).gsub(',', '.'),
 		:date	=> Time.now.to_i
 	)
 
 	redirect '/show'
 end
+
