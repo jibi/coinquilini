b256069a5915b60ef53480bd21d87bc867f187e7
jibi <jibi@paranoici.org>
2013-02-24 17:47:27 +0100

    fixed goddamn alignment

--- coinquilini.rb
+++ coinquilini.rb
@@ -6,31 +6,31 @@ require 'sequel'
 DB = Sequel.connect('sqlite://db')
 
 DB.create_table?(:users) do
-  primary_key	:id
+	primary_key :id
 
-  String	:name
-	String	:password
+	String      :name
+	String      :password
 end
 
 DB.create_table?(:lists) do
-  primary_key	:id
+	primary_key :id
 
-  String	:name
+	String      :name
 end
 
 DB.create_table?(:payments) do
-  primary_key	:id
+	primary_key :id
 
-  foreign_key	:user, :users
-  foreign_key	:list, :lists
+	foreign_key :user, :users
+	foreign_key :list, :lists
 
-  String	:what
-  Float		:sum
-  Integer	:date
+	String      :what
+	Float       :sum
+	Integer     :date
 end
 
-Users = DB[:users]
-Lists = DB[:lists]
+Users    = DB[:users]
+Lists    = DB[:lists]
 Payments = DB[:payments]
 
 module Sinatra
@@ -81,7 +81,7 @@ module Validate
 	def validate name, param
 		@fail_erb = {
 			:error => 'Something\'s missing.',
-			:msg	=> "Please specify #{name}"
+			:msg   => "Please specify #{name}"
 		}
 
 		halt erb(:fail) if params[param].nil? or params[param].empty?
@@ -112,8 +112,8 @@ module View
 	end
 
 	def build_summary_table(ind_tot, avg_tot)
-		summary	= ''
-		c = 0
+		summary = ''
+		c       = 0
 
 		ind_tot.each do |k,v|
 			d = (v - avg_tot).round(2)
@@ -130,7 +130,7 @@ module View
 
 	def build_payments_table(payments)
 		table = ''
-		c = 0
+		c     = 0
 
 		payments.each do |p|
 			table += '<tr' + (c.even? ? ' class=alt' : '') +
@@ -146,7 +146,7 @@ module View
 
 	def build_payme_table(debts)
 		table = ''
-		c = 0
+		c     = 0
 
 		debts.each do |d|
 			table += '<tr' + (c.even? ? ' class=alt' : '') +
@@ -197,7 +197,7 @@ end
 before '/admin/*' do
 	@fail_erb = {
 		:error => 'Admin area.',
-		:msg	=> "You are not admin"
+		:msg   => "You are not admin"
 	}
 
 	halt erb(:fail) if session[:user][:id] != 1
@@ -214,9 +214,9 @@ end
 
 post '/first_config' do
 	admin_password = validate('admin password', :admin_password)
-	name = validate('first user name', :name)
-	password = validate('first user password', :password)
-	list = validate('first list', :list)
+	name           = validate('first user name', :name)
+	password       = validate('first user password', :password)
+	list           = validate('first list', :list)
 
 	new_user('admin', admin_password)
 	new_user(name, password)
@@ -232,7 +232,7 @@ get '/auth' do
 end
 
 post '/auth' do
-	name = validate('username', :name)
+	name     = validate('username', :name)
 	password = validate('password', :password)
 
 	session[:user] = authenticate!(name, password)
@@ -254,7 +254,7 @@ get '/admin/new_user' do
 end
 
 post '/admin/new_user' do
-	name = validate('name',  :name)
+	name     = validate('name',  :name)
 	password = validate('password',  :password)
 
 	new_user(name, password)
@@ -275,19 +275,18 @@ post '/admin/new_list' do
 end
 
 get '/payments' do
-	list = params[:list] || Lists.first[:id]
+	list     = params[:list] || Lists.first[:id]
 
-	ind_tot	= Hash.new(0) # per user total
-	tot	= 0 # global total
+	ind_tot  = Hash.new(0) # per user total
+	tot      = 0 # global total
 
-	period	= params["period"] || Time.now.strftime("%Y %m")
-	year	= period[0..3].to_i
-	month	= period[5..6].to_i
-	users_n	= get_distinct_users.count
+	period   = params["period"] || Time.now.strftime("%Y %m")
+	year     = period[0..3].to_i
+	month    = period[5..6].to_i
+	users_n  = get_distinct_users.count
 
-	start_t = Time.mktime(year, month).to_i
-	end_t	= (month.eql?(12) ? Time.mktime(year + 1) :
-		  Time.mktime(year, month + 1)).to_i
+	start_t  = Time.mktime(year, month).to_i
+	end_t    = (month.eql?(12) ? Time.mktime(year + 1) : Time.mktime(year, month + 1)).to_i
 
 	payments = get_payments(list, start_t, end_t)
 	
@@ -306,15 +305,14 @@ get '/payments' do
 	end
 
 	debtmatrix = calc_debtmatrix ind_tot
-
-	avg_tot = (tot / users_n).round(2)
+	avg_tot    = (tot / users_n).round(2)
 
 	@payments_erb = {
-		:lists_list => build_lists_list(list),
-		:period_list	=> build_period_list,
-		:summary_table	=> build_summary_table(ind_tot, avg_tot),
-		:payments_table	=> build_payments_table(payments),
-		:payme_table	=> build_payme_table(debtmatrix)
+		:lists_list     => build_lists_list(list),
+		:period_list    => build_period_list,
+		:summary_table  => build_summary_table(ind_tot, avg_tot),
+		:payments_table => build_payments_table(payments),
+		:payme_table    => build_payme_table(debtmatrix)
 	}
 
 	erb :payments
@@ -322,8 +320,8 @@ end
 
 post '/' do
 	list = validate('list', :list)
-	what	= validate('what', :what)
-	sum	= validate('sum', :sum).gsub(',', '.')
+	what = validate('what', :what)
+	sum  = validate('sum', :sum).gsub(',', '.')
 
 	new_payment(session[:user][:id], what, sum, list)
 
