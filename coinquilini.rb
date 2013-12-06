78f74900a7e0b0a3d038988693bb7e383e1b0ab8
jibi <jibi@paranoici.org>
2013-12-06 11:11:28 +0100

    Retab coinquilini.rb

--- coinquilini.rb
+++ coinquilini.rb
@@ -7,27 +7,27 @@ require 'json'
 DB = Sequel.connect('sqlite://db')
 
 DB.create_table?(:users) do
-	primary_key :user_id
+  primary_key :user_id
 
-	String      :user_name
-	String      :user_password
+  String      :user_name
+  String      :user_password
 end
 
 DB.create_table?(:lists) do
-	primary_key :list_id
+  primary_key :list_id
 
-	String      :list_name
+  String      :list_name
 end
 
 DB.create_table?(:payments) do
-	primary_key :payment_id
+  primary_key :payment_id
 
-	foreign_key :payment_user, :users
-	foreign_key :payment_list, :lists
+  foreign_key :payment_user, :users
+  foreign_key :payment_list, :lists
 
-	String      :payment_what
-	Float       :payment_sum
-	Integer     :payment_date
+  String      :payment_what
+  Float       :payment_sum
+  Integer     :payment_date
 end
 
 Users    = DB[:users]
@@ -36,351 +36,351 @@ Payments = DB[:payments]
 
 module Sinatra
 module Db
-	def new_user(name, password)
-		Users.insert(:user_name => name, :user_password => password)
-	end
+  def new_user(name, password)
+    Users.insert(:user_name => name, :user_password => password)
+  end
 
-	def new_list(name)
-		Lists.insert(:list_name => name)
-	end
+  def new_list(name)
+    Lists.insert(:list_name => name)
+  end
 
-	def get_distinct_users(list, start_t, end_t)
-		Payments.select(:payment_user).where(:payment_date =>
-		  (start_t .. end_t), :payment_list => list).distinct
-	end
+  def get_distinct_users(list, start_t, end_t)
+    Payments.select(:payment_user).where(:payment_date =>
+      (start_t .. end_t), :payment_list => list).distinct
+  end
 
-	def new_payment(user, what, sum, list)
-		Payments.insert(:payment_user => user, :payment_what => what,
-		  :payment_sum => sum, :payment_date => Time.now.to_i, :payment_list => list)
-	end
+  def new_payment(user, what, sum, list)
+    Payments.insert(:payment_user => user, :payment_what => what,
+      :payment_sum => sum, :payment_date => Time.now.to_i, :payment_list => list)
+  end
 
-	def get_payments(list, start_t, end_t)
-		Payments.join(:users, :user_id => :payment_user).
-		  where(:payment_date => (start_t .. end_t), :payment_list => list)
-	end
+  def get_payments(list, start_t, end_t)
+    Payments.join(:users, :user_id => :payment_user).
+      where(:payment_date => (start_t .. end_t), :payment_list => list)
+  end
 
-	def delete_payment(pid)
-		p = Payments.select.where(:payment_id => pid)
+  def delete_payment(pid)
+    p = Payments.select.where(:payment_id => pid)
 
-		raise 'No such payment' if p.count.zero?
-		raise 'Cannot delete this payment (it\'s not yours)' if
-		  not p.first[:payment_user].to_i.eql?(session[:user][:id])
+    raise 'No such payment' if p.count.zero?
+    raise 'Cannot delete this payment (it\'s not yours)' if
+      not p.first[:payment_user].to_i.eql?(session[:user][:id])
 
-		p.delete
-	end
+    p.delete
+  end
 end
 
 module DebtMatrix
-	def calc_debtmatrix exp, debts = []
-		stat = {}
+  def calc_debtmatrix exp, debts = []
+    stat = {}
 
-		exp = exp.reject { |k,v| v.zero? }
-		return debts if exp.size == 1
+    exp = exp.reject { |k,v| v.zero? }
+    return debts if exp.size == 1
 
-		avg = exp.reduce(0) { |sum, (_, v)| sum + v }.to_f / exp.size
-		exp.each { |k, v| stat[k] = v - avg }
+    avg = exp.reduce(0) { |sum, (_, v)| sum + v }.to_f / exp.size
+    exp.each { |k, v| stat[k] = v - avg }
 
-		min_recv, max_recv = [stat.min_by(&:last), stat.max_by(&:last)]
+    min_recv, max_recv = [stat.min_by(&:last), stat.max_by(&:last)]
 
-		exp[max_recv[0]] -= min_recv[1].abs
-		exp[min_recv[0]] = 0
+    exp[max_recv[0]] -= min_recv[1].abs
+    exp[min_recv[0]] = 0
 
-		debts << { :from => min_recv[0], :to => max_recv[0],
-		  :what => min_recv[1].abs.round(2)}
+    debts << { :from => min_recv[0], :to => max_recv[0],
+      :what => min_recv[1].abs.round(2)}
 
-		calc_debtmatrix exp, debts
-	end
+    calc_debtmatrix exp, debts
+  end
 end
 
 module Validate
-	def validate name, param
-		response = {
-			:status => 'error',
-			:msg   => "Please specify #{name}"
-		}
+  def validate name, param
+    response = {
+      :status => 'error',
+      :msg   => "Please specify #{name}"
+    }
 
-		halt response.to_json if params[param].nil? or params[param].empty?
+    halt response.to_json if params[param].nil? or params[param].empty?
 
-		params[param]
-	end
+    params[param]
+  end
 end
 
 module View
-	def build_lists_list(default=1)
-		list = ''
+  def build_lists_list(default=1)
+    list = ''
 
-		Lists.each do |l|
-			list += "<option value='#{l[:list_id]}'" +
-			  "#{" selected='selected'" if 
-			    default.to_i == l[:list_id]} >#{l[:list_name]}</option>"
-		end
+    Lists.each do |l|
+      list += "<option value='#{l[:list_id]}'" +
+        "#{" selected='selected'" if 
+          default.to_i == l[:list_id]} >#{l[:list_name]}</option>"
+    end
 
-		list
-	end
+    list
+  end
 
-	def build_period_list
-		list = ''
+  def build_period_list
+    list = ''
 
-		DB["SELECT DISTINCT strftime('%Y %m',payment_date, 'unixepoch') " +
-		  "AS ym FROM payments"].each do |d|
-			list += "<option value='#{d[:ym]}'>#{d[:ym]}</option>"
-		end
+    DB["SELECT DISTINCT strftime('%Y %m',payment_date, 'unixepoch') " +
+      "AS ym FROM payments"].each do |d|
+      list += "<option value='#{d[:ym]}'>#{d[:ym]}</option>"
+    end
 
-		list
-	end
+    list
+  end
 
-	def build_summary_table(ind_tot, avg_tot)
-		summary = ''
-		c       = 0
+  def build_summary_table(ind_tot, avg_tot)
+    summary = ''
+    c       = 0
 
-		ind_tot.each do |k,v|
-			d = (v - avg_tot).round(2)
+    ind_tot.each do |k,v|
+      d = (v - avg_tot).round(2)
 
-			summary += '<tr class=' + (d >= 0 ? 'success' : 'danger' ) + '><td>' + k.to_s +
-				'</td><td>' + v.round(2).to_s + '</td><td class=>' + d.to_s + '</td></tr>'
+      summary += '<tr class=' + (d >= 0 ? 'success' : 'danger' ) + '><td>' + k.to_s +
+        '</td><td>' + v.round(2).to_s + '</td><td class=>' + d.to_s + '</td></tr>'
 
-			c = c + 1
-		end
+      c = c + 1
+    end
 
-		summary
-	end
+    summary
+  end
 
-	def build_payments_table(payments)
-		table = ''
-		c     = 0
+  def build_payments_table(payments)
+    table = ''
+    c     = 0
 
-		payments.each do |p|
-			table += '<tr' + (c.even? ? ' class=alt' : '') +
-				'><td>' + Time.at(p[:payment_date]).strftime('%d %b, %H:%M') +
-				'</td><td>' + p[:user_name] + '</td><td>' + p[:payment_what] +
-				'</td><td>' + p[:payment_sum].to_s + '</td>' +
-				'</td><td><a href=/delete?pid=' + p[:payment_id].to_s + '&uid=' +
-				p[:payment_user].to_s + '&lid=' + p[:payment_list].to_s + '>X</a></td></tr>'
+    payments.each do |p|
+      table += '<tr' + (c.even? ? ' class=alt' : '') +
+        '><td>' + Time.at(p[:payment_date]).strftime('%d %b, %H:%M') +
+        '</td><td>' + p[:user_name] + '</td><td>' + p[:payment_what] +
+        '</td><td>' + p[:payment_sum].to_s + '</td>' +
+        '</td><td><a href=/delete?pid=' + p[:payment_id].to_s + '&uid=' +
+        p[:payment_user].to_s + '&lid=' + p[:payment_list].to_s + '>X</a></td></tr>'
 
-			c = c + 1
-		end
+      c = c + 1
+    end
 
-		table
-	end
+    table
+  end
 
-	def build_payme_table(debts)
-		table = ''
-		c     = 0
+  def build_payme_table(debts)
+    table = ''
+    c     = 0
 
-		debts.each do |d|
-			table += '<tr' + (c.even? ? ' class=alt' : '') +
-				'><td>' + d[:from] +
-				'</td><td>' + d[:what].to_s + '</td><td>' + d[:to] + '</td></tr>'
+    debts.each do |d|
+      table += '<tr' + (c.even? ? ' class=alt' : '') +
+        '><td>' + d[:from] +
+        '</td><td>' + d[:what].to_s + '</td><td>' + d[:to] + '</td></tr>'
 
-			c = c + 1
+      c = c + 1
 
-		end
+    end
 
-		table
-	end
+    table
+  end
 
-	def menu_item(link, name)
-		active = (request.path_info == link ? "class=\"active\"" : "")
-		"<li #{active}><a href=\"#{link}\">#{name}</a></li>"
-	end
+  def menu_item(link, name)
+    active = (request.path_info == link ? "class=\"active\"" : "")
+    "<li #{active}><a href=\"#{link}\">#{name}</a></li>"
+  end
 end
 
 module Auth
-	def authenticate!(name, password)
-		res = Users.where(:user_name => name, :user_password => password)
-		if res.count.nonzero?
-			session[:user] = {:name => name, :id => res.first[:user_id]}
-		end
-	end
+  def authenticate!(name, password)
+    res = Users.where(:user_name => name, :user_password => password)
+    if res.count.nonzero?
+      session[:user] = {:name => name, :id => res.first[:user_id]}
+    end
+  end
 
-	def authenticated?
-		not session[:user].nil?
-	end
+  def authenticated?
+    not session[:user].nil?
+  end
 end
 end
 
 configure do
-	helpers Sinatra::Db
-	helpers Sinatra::DebtMatrix
-	helpers Sinatra::Validate
-	helpers Sinatra::View
-	helpers Sinatra::Auth
+  helpers Sinatra::Db
+  helpers Sinatra::DebtMatrix
+  helpers Sinatra::Validate
+  helpers Sinatra::View
+  helpers Sinatra::Auth
 
-	set :server, :puma
-	set :bind, "0.0.0.0"
-	enable :sessions
+  set :server, :puma
+  set :bind, "0.0.0.0"
+  enable :sessions
 end
 
 before do
-	pass if request.path_info.split('?')[0].match(/^\/first_config|^\/auth/)
+  pass if request.path_info.split('?')[0].match(/^\/first_config|^\/auth/)
 
-	redirect '/first_config' if Users.count.zero? #which means no admin user.
-	redirect '/auth' if session[:user].nil?
+  redirect '/first_config' if Users.count.zero? #which means no admin user.
+  redirect '/auth' if session[:user].nil?
 end
 
 before '/admin/*' do
-	@fail_erb = {
-		:error => 'Admin area.',
-		:msg   => "You are not admin"
-	}
+  @fail_erb = {
+    :error => 'Admin area.',
+    :msg   => "You are not admin"
+  }
 
-	halt erb(:fail) if session[:user][:id] != 1
+  halt erb(:fail) if session[:user][:id] != 1
 end
 
 get '/' do
-	@pay_form_erb = { :lists_list => build_lists_list }
-	erb :pay_form
+  @pay_form_erb = { :lists_list => build_lists_list }
+  erb :pay_form
 end
 
 get '/first_config' do
-	erb :first_config
+  erb :first_config
 end
 
 post '/first_config' do
-	admin_password = validate('admin password', :admin_password)
-	name           = validate('first user name', :name)
-	password       = validate('first user password', :password)
-	list           = validate('first list', :list)
+  admin_password = validate('admin password', :admin_password)
+  name           = validate('first user name', :name)
+  password       = validate('first user password', :password)
+  list           = validate('first list', :list)
 
-	new_user('admin', admin_password)
-	new_user(name, password)
+  new_user('admin', admin_password)
+  new_user(name, password)
 
-	new_list(list)
+  new_list(list)
 
-	redirect '/auth'
+  redirect '/auth'
 end
 
 get '/auth' do
-	session.clear
-	erb :auth
+  session.clear
+  erb :auth
 end
 
 post '/auth' do
-	name     = validate('username', :name)
-	password = validate('password', :password)
+  name     = validate('username', :name)
+  password = validate('password', :password)
 
-	session[:user] = authenticate!(name, password)
+  session[:user] = authenticate!(name, password)
 
-	if authenticated?
-		redirect '/'
-	else
-		@fail_erb = {
-			:error => 'Wrong passphrase',
-			:msg   => "But you can try <a href=/auth>again</a>"
-		}
+  if authenticated?
+    redirect '/'
+  else
+    @fail_erb = {
+      :error => 'Wrong passphrase',
+      :msg   => "But you can try <a href=/auth>again</a>"
+    }
 
-		halt erb(:fail)
-	end
+    halt erb(:fail)
+  end
 end
 
 get '/admin/new_user' do
-	erb :new_user
+  erb :new_user
 end
 
 post '/admin/new_user' do
-	name     = validate('name',  :name)
-	password = validate('password',  :password)
+  name     = validate('name',  :name)
+  password = validate('password',  :password)
 
-	new_user(name, password)
+  new_user(name, password)
 
-	redirect '/'
+  redirect '/'
 end
 
 get '/admin/new_list' do
-	erb :new_list
+  erb :new_list
 end
 
 post '/admin/new_list' do
-	name = validate('list name',  :name)
+  name = validate('list name',  :name)
 
-	new_list(name)
+  new_list(name)
 
-	redirect '/'
+  redirect '/'
 end
 
 get '/payments' do
-	list     = params[:list] || Lists.first[:list_id]
+  list     = params[:list] || Lists.first[:list_id]
 
-	ind_tot  = Hash.new(0) # per user total
-	tot      = 0 # global total
+  ind_tot  = Hash.new(0) # per user total
+  tot      = 0 # global total
 
-	period   = params["period"] || Time.now.strftime("%Y %m")
-	year     = period[0..3].to_i
-	month    = period[5..6].to_i
+  period   = params["period"] || Time.now.strftime("%Y %m")
+  year     = period[0..3].to_i
+  month    = period[5..6].to_i
 
-	start_t  = Time.mktime(year, month).to_i
-	end_t    = (month.eql?(12) ? Time.mktime(year + 1) : Time.mktime(year, month + 1)).to_i
+  start_t  = Time.mktime(year, month).to_i
+  end_t    = (month.eql?(12) ? Time.mktime(year + 1) : Time.mktime(year, month + 1)).to_i
 
-	users_n  = get_distinct_users(list, start_t, end_t).count
-	payments = get_payments(list, start_t, end_t)
+  users_n  = get_distinct_users(list, start_t, end_t).count
+  payments = get_payments(list, start_t, end_t)
 
-	@payments_erb = {
-		:lists_list     => build_lists_list(list),
-		:period_list    => build_period_list,
-	}
-	
-	if not payments.count.zero?
-		payments.each do |p|
-			ind_tot[p[:user_name]] += p[:payment_sum].to_f.round(2)
-			tot                    += p[:payment_sum].to_f.round(2)
-		end
+  @payments_erb = {
+    :lists_list     => build_lists_list(list),
+    :period_list    => build_period_list,
+  }
 
-		debtmatrix = calc_debtmatrix ind_tot
-		avg_tot    = (tot / users_n).round(2)
+  if not payments.count.zero?
+    payments.each do |p|
+      ind_tot[p[:user_name]] += p[:payment_sum].to_f.round(2)
+      tot                    += p[:payment_sum].to_f.round(2)
+    end
 
-		@payments_erb.merge!({
-			:summary_table  => build_summary_table(ind_tot, avg_tot),
-			:payments_table => build_payments_table(payments),
-			:payme_table    => build_payme_table(debtmatrix)
-		})
-	end
+    debtmatrix = calc_debtmatrix ind_tot
+    avg_tot    = (tot / users_n).round(2)
 
-	erb :payments
+    @payments_erb.merge!({
+      :summary_table  => build_summary_table(ind_tot, avg_tot),
+      :payments_table => build_payments_table(payments),
+      :payme_table    => build_payme_table(debtmatrix)
+    })
+  end
+
+  erb :payments
 end
 
 post '/' do
-	list = validate('list', :list)
-	what = validate('what', :what)
-	sum  = validate('sum', :sum).gsub(',', '.').to_f
+  list = validate('list', :list)
+  what = validate('what', :what)
+  sum  = validate('sum', :sum).gsub(',', '.').to_f
 
-	if sum.to_i < 0
-		response = {
-			:status => 'error',
-			:msg    => 'Negative sum.<br>Doesn\'t make sense.'
-		}
+  if sum.to_i < 0
+    response = {
+      :status => 'error',
+      :msg    => 'Negative sum.<br>Doesn\'t make sense.'
+    }
 
-		halt response.to_json
-	end
+    halt response.to_json
+  end
 
-	new_payment(session[:user][:id], what, sum, list)
+  new_payment(session[:user][:id], what, sum, list)
 
-	response = {
-		:status => "ok",
-		:msg    => "Successfully added #{session[:user][:name]}'s payment:<br>#{sum}€ for #{what}"
-	}
+  response = {
+    :status => "ok",
+    :msg    => "Successfully added #{session[:user][:name]}'s payment:<br>#{sum}€ for #{what}"
+  }
 
-	response.to_json
+  response.to_json
 end
 
 get '/delete' do
-	pid = validate('payement id', :pid)
+  pid = validate('payement id', :pid)
 
-	begin
-		delete_payment(pid).eql?(-1)
-	rescue Exception => e
-		@fail_erb = {
-			:error => 'Fail deleting payment.',
-			:msg   => e.message + '<br>Go <a href=/>back</a>'
-		}
+  begin
+    delete_payment(pid).eql?(-1)
+  rescue Exception => e
+    @fail_erb = {
+      :error => 'Fail deleting payment.',
+      :msg   => e.message + '<br>Go <a href=/>back</a>'
+    }
 
-		halt erb(:fail)
-	end
+    halt erb(:fail)
+  end
 
-	redirect '/'
+  redirect '/'
 end
 
 get '/logout' do
-	session.clear
-	redirect '/auth'
+  session.clear
+  redirect '/auth'
 end
 
